394ff3cd628fc73ca0bff4a1bdd10988f69da415
majadev <mgedda@live.se>
2025-04-09 20:40:59 +0200

    get images from fb posts

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ app.get("/api/secret", (req, res) => {
     res.json({ mail: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS });
 });
 
-// route för att hämta inlägg från Facebook
 app.get("/api/facebook", async (req, res) => {
     const pageId = "661527893700232"; 
     const accessToken = process.env.FACEBOOK_ACCESS_TOKEN;
-
-    const url = `https://graph.facebook.com/v18.0/${pageId}/posts?access_token=${accessToken}`;
+    const url = `https://graph.facebook.com/v18.0/${pageId}/posts?fields=id,message,attachments&access_token=${accessToken}`;
 
     try {
-        // förfrågan till Facebooks API
         const response = await axios.get(url);
 
-        // Skicka tillbaka inläggen till frontend
-        res.status(200).json(response.data);
+        const posts = response.data.data.map(post => {
+            const media = post.attachments ? post.attachments.data.map(att => att.media) : null;
+            return { ...post, media };
+        });
+        res.status(200).json(posts);
     } catch (error) {
         console.error("Fel vid hämtning av Facebook-inlägg:", error);
         res.status(500).json({ error: "Något gick fel när vi hämtade inläggen" });
