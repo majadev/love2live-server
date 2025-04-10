b07546239cd4040edb661bc08194432b2a82b108
majadev <mgedda@live.se>
2025-04-10 15:32:32 +0200

    add next pages to res in fb api call

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.get("/api/secret", (req, res) => {
 });
 
 app.get("/api/facebook", async (req, res) => {
-    const pageId = "661527893700232"; 
+    const pageId = "661527893700232";
     const accessToken = process.env.FACEBOOK_ACCESS_TOKEN;
-    const url = `https://graph.facebook.com/v18.0/${pageId}/posts?fields=id,message,created_time,permalink_url,attachments{media,subattachments{media}}&access_token=${accessToken}`;
+    const url = `https://graph.facebook.com/v18.0/${pageId}/posts?fields=id,message,created_time,permalink_url,attachments{media,subattachments{media}}&limit=10&access_token=${accessToken}`;
 
     try {
         const response = await axios.get(url);
@@ -29,7 +29,11 @@ app.get("/api/facebook", async (req, res) => {
             const media = post.attachments ? post.attachments.data.map(att => att.media) : null;
             return { ...post, media };
         });
-        res.status(200).json(posts);
+
+        res.status(200).json({
+            posts,
+            nextPage: response.data.paging?.next || null,
+        });
     } catch (error) {
         console.error("Fel vid hämtning av Facebook-inlägg:", error);
         res.status(500).json({ error: "Något gick fel när vi hämtade inläggen" });
